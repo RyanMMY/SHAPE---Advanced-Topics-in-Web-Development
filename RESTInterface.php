46d5afc3a418b57db1dea89f6516eb5c1d8b7498
Ryan Mok <86401891+RyanMMY@users.noreply.github.com>
2021-06-24 17:06:15 +0800

    Delete RESTInterface.php

--- RESTInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface RESTInterface{
-	public function restGet($urlSegment);
-    public function restPut($urlSegment);
-    public function restDelete($urlSegment);
-    public function restPost($urlSegment);
-}
